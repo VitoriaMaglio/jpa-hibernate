891c84e9501fb5093b972450f8918c3147885191
VitoriaMaglio <vitoriamaglio@gmail.com>
2025-09-25 08:37:25 -0300

    Buscando dados no banco.

--- src/main/java/com/estudando/Main.java
+++ src/main/java/com/estudando/Main.java
@@ -8,17 +8,32 @@ import javax.persistence.Persistence;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
-        Pessoa p2 = new Pessoa(null, "Joaquim Torres", "joaquim@gmail.com");
-        Pessoa p3 = new Pessoa(null, "Ana Maria", "ana@gmail.com");
+        //Quando vc insere um obj na base de dados vc pode tirar a instanciação dele do seu código pq ele jhá está inserido no banco
+        //Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
+        //Pessoa p2 = new Pessoa(null, "Joaquim Torres", "joaquim@gmail.com");
+        //Pessoa p3 = new Pessoa(null, "Ana Maria", "ana@gmail.com");
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
         EntityManager em = emf.createEntityManager();
         em.getTransaction().begin();
-        em.persist(p1);
-        em.persist(p2);
-        em.persist(p3);
-        em.getTransaction().commit();
-        System.out.println(p1);
+        //em.persist(p1);
+        //em.persist(p2);
+        //em.persist(p3);
+        //em.getTransaction().commit();
+
         System.out.println("Pronto");
+
+
+        //BUSCAR DADO NO BANCO
+        Pessoa p = em.find(Pessoa.class, 2);
+        System.out.println(p);
+
+        //entidade monitorada na contexto de persistência
+        //para agapar um obj persistido no banco ele tem q estar monitorada, que é um obj que acbou de ser inserido ou quando vc ainda não fechou o EntityManagr e deve recuperar com find o obj
+        //transação,declara,busca e depois remove,commit
+
+
+
+        em.close();
+        emf.close();
 
     }}
